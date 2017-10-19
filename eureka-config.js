ac9ce21b9fd23ec16f4c654d65fffe7ca90d2546
韩学鹏 <hanxuepeng001@163.com>
2017-10-19 08:54:45 +0800

    增加服务更新事件监听

---
+++ eureka-config.js
@@ -0,0 +1,36 @@
+const project_info = require('./package.json');
+module.exports = {
+	/**
+	 * eureka 服务端配置
+	 * @type {Object}
+	 */
+	eureka: {
+		serviceUrls: {
+			default: [
+			'http://admin:admin@192.168.0.159:8761/eureka/apps/', 
+			'http://admin:admin@127.0.0.1:8761/eureka/apps/', 
+			'http://admin:admin@192.168.0.161:8761/eureka/apps/'
+			]
+		}
+		// "host": "admin:admin@192.168.0.159",
+		// "port": 8761,
+		// "servicePath": "/eureka/apps/"
+	},
+
+	/**
+	 * eureka 客户端配置
+	 * @type {Object}
+	 */
+	instance: {
+		"app": "thomas-service",
+	    "port": {
+	      "$": 3000,
+	      "@enabled": "true"
+	    },
+	    "metadata": {
+	    	"zone": "primary",
+	    	"env": process.env.NODE_ENV,
+	    	"version": project_info.version
+	    }
+	}
+};
--- example.js
+++ example.js
@@ -1,36 +1,24 @@
+const _ = require("lodash");
 const EurekaClient = require('./lib/index');
-const os = require('os');
+const config = require('./eureka-config');
 
-let hostname = os.hostname();
-console.log(hostname);
-let eureka_client = new EurekaClient({
-	eureka: {
-		// serviceUrls: {
-		// 	default: ['http://admin:admin@192.168.2.155:8761/eureka/apps/', 'http://admin:admin@192.168.2.168:8761/eureka/apps/', 'http://admin:admin@192.168.2.157:8761/eureka/apps/']
-		// }
-		host: 'admin:admin@192.168.2.168',
-		port: 8761,
-		servicePath: '/eureka/apps/'
-	},
-	instance: {
-	    app: 'thomas-service',
-	    vipAddress: 'thomas-service',
-	    hostName: '192.168.2.184',
-	    ipAddr: '192.168.2.184',
-	    port: {
-	      '$': 3000,
-	      '@enabled': 'true'
-	    },
-	    metadata: {
-	    	zone: 'test',
-	    	env: process.env.NODE_ENV,
-	    	version: '0.0.1',
-	    	other: 'thomas'
-	    }
-	}
-});
+let eureka_client = new EurekaClient(config);
 
 eureka_client.logger.level('debug');
+
+//事件监听
+let updatedListener = (apps) => {
+	_.forEach(apps, (pathes, appName) => {
+		console.log(`名称=${appName}`);
+		_.forEach(pathes, (path, key) => {
+			console.log(`路径=${JSON.stringify(path)}`);
+		});
+	});
+}
+
+
+eureka_client.onUpdated(updatedListener);
+
 eureka_client.start(function(error){
 	console.log(error || '启动成功！');
 });
